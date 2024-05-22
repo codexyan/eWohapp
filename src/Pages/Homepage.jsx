e902ee446bdfa5cfd09ecdce6e7d9e33244d9e78
codexyan <mdcodeid@gmail.com>
2024-05-22 20:51:20 +0700

    landing page update

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,50 +1,29 @@
 import React from "react";
-import Counter from "../components/Fragments/Counter";
-import { useRef } from "react";
-import { useState } from "react";
-import { useEffect } from "react";
+import { FaWhatsapp } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Homepage() {
-  const inputRef = useRef(null);
-
-  const handleClick = () => {
-    inputRef.current.focus();
-  };
-
-  const [clicked, setClicked] = useState(false);
-
-  useEffect(() => {
-    if (clicked) {
-      setClicked(alert("Halo guys!"));
-    }
-  }, [clicked]);
-
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-5xl mb-4 font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
-          Welcome To Teras Depan
+      <div className="flex flex-col items-center justify-center h-screen mt-10">
+        <img src="../../public/images/hero-images.svg" alt="hero" className="min-xl:w-96 mb-10" />
+        <h1 className="text-3xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-amber-900 to-amber-600">
+          Undangan eWoh Digital
         </h1>
-        <Counter></Counter>
-        <div className="mt-5 flex flex-row items-center justify-center gap-4">
-          <input
-            type="text"
-            className="border rounded-full py-2 px-5"
-            ref={inputRef}
-          />
-          <button
-            onClick={handleClick}
-            className="bg-green-800 py-2 px-6 text-white rounded-full shadow-sm"
-          >
-            Fokuskan Input
-          </button>
+        <div className="mt-2 flex flex-col items-center justify-center gap-2">
+          <p className="text-md text-center text-slate-700">
+            Pembuatan undangan digital berbasis website. Digitalisasikan undanganmu dan saatnya kamu bergabung bersama yang lainnya.
+          </p>
+          <p className="text-lg text-slate-400 italic">#pakaieWohAja</p>
         </div>
-        <button
-          onClick={() => setClicked(true)}
-          className="bg-orange-800 py-2 px-6 mt-5 font-bold text-white rounded-full shadow-sm"
+        <Link
+        to="/"
+          className="bg-orange-800 py-3 px-8 mt-5 font-bold text-white rounded-full shadow-sm flex flex-row gap-3 items-center justify-center"
         >
-          Sapa Dia!
-        </button>
+          <FaWhatsapp className="text-3xl" />
+          <p>Hubungi Kami</p>
+        </Link>
+        <p className="text-sm text-slate-400 my-10 text-center">Copyright © 2024 | eWohapp All Rights Reserved</p>
       </div>
     </>
   );
