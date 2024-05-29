d29870f535fc95e27850a0a36d28efef0dbf45d1
codexyan <mdcodeid@gmail.com>
2024-05-29 11:27:15 +0700

    update fontstyle

--- src/Pages/SendInvitation.jsx
+++ src/Pages/SendInvitation.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { Button } from "@mui/material";
 import { useSearchParams } from "react-router-dom";
+
+// Icons
+import { FaEnvelopeOpenText } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function SendInvitationPage() {
   const [searchParams] = useSearchParams();
@@ -10,24 +13,33 @@ export default function SendInvitationPage() {
     <>
       <div className="min-w-screen">
         {/* Hero Section */}
-        <div id="hero" className="min-h-screen flex flex-col bg-hero-pattern2 bg-cover bg-no-repeat bg-center justify-around">
-            <div className="text-white text-center flex flex-col justify-center border basis-3/4">
-              <p className="text-sm font-normal">YTH Bapak/Ibu/Saudara/i :</p>
-              <p className="member font-righteous">{guestName}</p>
-            </div>
-            <div className="wrap-content text-center flex flex-col gap-10 py-8 justify-end items-center basis-1/ border">
-              <div className="content">
-                <h1 className="text-white font-righteous">
-                  We Invite You To Our Wedding
+        <div
+          id="hero"
+          className="min-h-screen flex flex-col bg-hero-pattern2 bg-cover bg-no-repeat bg-center justify-around"
+        >
+          <div className="text-white text-center flex flex-col justify-center basis-3/4">
+            <p className="text-sm font-semibold">YTH Bapak/Ibu/Saudara/i :</p>
+            <p className="member font-righteous">{guestName}</p>
+          </div>
+          <div className="wrap-content text-center flex flex-col gap-10 py-8 justify-end items-center basis-1/">
+            <div className="content">
+              <h1 className="text-white font-righteous">
+                We Invite You To Our Wedding
+              </h1>
+              <div className="couple font-playfair text-5xl">
+                <h1 className="text-white font-playfair shadow">
+                  Ary & Fernanda{" "}
                 </h1>
-                <div className="couple font-playfair text-5xl">
-                  <h1 className="text-white">Putra & Putri</h1>
-                </div>
               </div>
-              <Button variant="contained" type="button">
-                Buka Undangan
-              </Button>
             </div>
+            <Link
+              to="/invitation"
+              className="flex flex-row justify-center items-center gap-2 bg-amber-900 px-8 py-3 text-white rounded-full shadow hover:bg-amber-950 transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg"
+            >
+              <FaEnvelopeOpenText />
+              Buka Undangan
+            </Link>
+          </div>
         </div>
       </div>
     </>
